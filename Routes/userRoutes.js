6e83bac94f540bc39f70f02a4b4901a9ea55b761
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-10-16 14:24:44 +0500

    change vercel

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const usersController = require('../Controller/usersController')
 
-router.get('/', usersController.getUser);
+router.get('/', (req ,res)=>{
+    res.send("hello from api/user");
+});
 router.post('/', usersController.createUser);
 router.put('/:id', usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
